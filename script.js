18608ceff5a2508131937fd64a3d4cc1c5d7452b
Andrew Raines <rainesand26@gmail.com>
2020-06-06 20:34:38 -0400

    Finished putting together the logic for the generator

--- script.js
+++ script.js
@@ -1,47 +1,83 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
 // Write password to the #password input
 // var confirmCapital = confirm("would you like to include capital letters?");
 // var confirmLowercase = confirm("would you like to include lowercase letters?");
 // var confirmNumber = confirm("would you like to include numbers?")
+function writePassword() {
 
+var specialChar = "!@#$%&*?";
+var numberChar = "1234567890";
+var alphabetChar = "abcdefghijklmnopqrstuvwxyz";
+var capitalChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var newPassword = "";
 
 function generatePassword() {
-  // possible characters included
-  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
  
-  var passwordLength = prompt("how long would you like your password to be?");
- 
-  var newPassword = "";
-
-  if (passwordLength  > 7 && passwordLength < 129  ) {
-    var confirmCapital = confirm("would you like to include capital letters?");
-      if (confirmCapital === true) {
-        newPassword
-      }
+    var passwordLength = prompt("how long would you like your password to be?");
+     if (passwordLength  > 7 && passwordLength < 129  ) {
+  
+        var confirmCapital = confirm("would you like to include capital letters?");
+        var confirmSpecial = confirm("would you like to use special characters?");
+        var confirmNumber = confirm("would you like to include numbers?");
+          if (confirmCapital === true && confirmSpecial === true && confirmNumber === true) {
+            for (var i = 0; i < passwordLength/4; i++){
+              newPassword = newPassword + alphabetChar.charAt(Math.floor(Math.random() * Math.floor(alphabetChar.length-1))) + capitalChar.charAt(Math.floor(Math.random() * Math.floor(capitalChar.length-1))) + numberChar.charAt(Math.floor(Math.random() * Math.floor(numberChar.length-1))) + specialChar.charAt(Math.floor(Math.random() * Math.floor(specialChar.length-1)));
+            }
+          }
+          else if (confirmCapital === true && confirmSpecial === true && confirmNumber !== true) {
+            for (var i = 0; i < passwordLength/3; i++){
+              newPassword = newPassword + alphabetChar.charAt(Math.floor(Math.random() * Math.floor(alphabetChar.length-1))) + capitalChar.charAt(Math.floor(Math.random() * Math.floor(capitalChar.length-1))) + specialChar.charAt(Math.floor(Math.random() * Math.floor(specialChar.length-1)));
+            }
+          }
+          else if (confirmCapital === true && confirmSpecial !== true && confirmNumber === true) {
+            for (var i = 0; i < passwordLength/3; i++){
+              newPassword = newPassword + alphabetChar.charAt(Math.floor(Math.random() * Math.floor(alphabetChar.length-1))) + capitalChar.charAt(Math.floor(Math.random() * Math.floor(capitalChar.length-1))) + numberChar.charAt(Math.floor(Math.random() * Math.floor(numberChar.length-1)));
+            }
+          }
+          else if (confirmCapital !== true && confirmSpecial === true && confirmNumber === true) {
+            for (var i = 0; i < passwordLength/3; i++){
+              newPassword = newPassword + alphabetChar.charAt(Math.floor(Math.random() * Math.floor(alphabetChar.length-1))) + numberChar.charAt(Math.floor(Math.random() * Math.floor(numberChar.length-1))) + specialChar.charAt(Math.floor(Math.random() * Math.floor(specialChar.length-1)));
+            }
+          }
+          else if (confirmCapital !== true && confirmSpecial !== true && confirmNumber === true) {
+            for (var i = 0; i < passwordLength/2; i++){
+              newPassword = newPassword + alphabetChar.charAt(Math.floor(Math.random() * Math.floor(alphabetChar.length-1))) + numberChar.charAt(Math.floor(Math.random() * Math.floor(numberChar.length-1)));
+            }
+          }
+          else if (confirmCapital !== true && confirmSpecial === true && confirmNumber !== true) {
+            for (var i = 0; i < passwordLength/2; i++){ 
+            newPassword = newPassword + alphabetChar.charAt(Math.floor(Math.random() * Math.floor(alphabetChar.length-1))) + specialChar.charAt(Math.floor(Math.random() * Math.floor(specialChar.length-1)));
+            }
+          }
+          else if (confirmCapital === true && confirmSpecial !== true && confirmNumber !== true) {
+            for (var i = 0; i < passwordLength/2; i++){
+            newPassword = newPassword + alphabetChar.charAt(Math.floor(Math.random() * Math.floor(alphabetChar.length-1))) + capitalChar.charAt(Math.floor(Math.random() * Math.floor(capitalChar.length-1)))
+            }
+          }
+          else {
+          newPassword = alphabetChar[i];
+          }
     }
+  
+    else {
+    alert("password must be at least 8 characters and at most 128 characters.");
+    }
+  
+    // for (var i = 0; i < passwordLength; i++){
+    //   newPassword = newPassword + alphabetChar.charAt(Math.floor(Math.random() * Math.floor(alphabetChar.length-1))) + capitalChar.charAt(Math.floor(Math.random() * Math.floor(capitalChar.length-1))) + numberChar.charAt(Math.floor(Math.random() * Math.floor(numberChar.length-1))) + specialChar.charAt(Math.floor(Math.random() * Math.floor(specialChar.length-1)));
+    // }
+  }
+  
+var password = generatePassword() ;
 
- else {
-   alert("password must be at least 8 characters and at most 128 characters.");
- }
+var passwordText = document.querySelector("#password");
+passwordText.value = password;
 
  
-// RNG for the password generator
-for (var i = 0; i < passwordLength; i++){
-  newPassword = newPassword + characters.charAt(Math.floor(Math.random() * Math.floor(characters.length-1)));
+console.log(newPassword);
 }
-
-console.log(newPassword)
-}
-
-function writePassword() {
-  var password = generatePassword() ;
-}
-
-
-
   // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
